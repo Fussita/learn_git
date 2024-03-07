fe2d1fc67e24b5aff07d704a1f74095878ac8da5
Fussita <110612202+Fussita@users.noreply.github.com>
2024-03-07 16:31:30 -0400

    d

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WsRoomsModule } from './ws-rooms/ws-rooms.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    WsRoomsModule
+    WsRoomsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
   ],
   controllers: [],
   providers: [],
