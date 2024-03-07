2e095c66bf30b971589143a98d7e85edba9ea2d9
Fussita <110612202+Fussita@users.noreply.github.com>
2024-03-07 12:22:00 -0400

    d

--- src/ws-rooms/ws-rooms.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WsRoomsService } from './ws-rooms.service';
-import { WsRoomsGateway } from './ws-rooms.gateway';
-
-@Module({
-  providers: [WsRoomsGateway, WsRoomsService],
-})
-export class WsRoomsModule {}
